a272f8e9d9a7f931a83bde374679ec7b871194b0
David Klein <david-klein@svws-nrw.de>
2026-06-08 16:22:42 +0200

    neue Datei:     clone_db_in_docker_container.sh 	geändert:       delete_all_docker_container.sh 	geändert:       delete_all_svws_dbs.sh 	geändert:       generate_svws_userpasswords.sh 	geändert:       install_svws_schild3_svws-tools_testserver.sh 	geändert:       install_svws_testserver_docker.sh 	geändert:       install_svws_testserver_linuxinstaller.sh 	geändert:       install_weblupo_testserver.sh 	geändert:       install_wenom_testserver.sh

--- Fachberatung_Anleitungen/schulungsserver/Installationsskripte/install_svws_testserver_docker.sh
+++ Fachberatung_Anleitungen/schulungsserver/Installationsskripte/install_svws_testserver_docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#######################################################
-### Das Skript erstellt einen SVWS-Docker-Container ###
-#######################################################
+###############################################
+### Das Skript löscht alle Docker Container ###
+###############################################
 #  
-# Copyright (c) $(date +%Y)
+# Copyright (c) (Mai 2026) Theo Richter & David Klein
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -22,7 +22,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-#######################################################
+###############################################
 
 # Standardwerte initialisieren
 FOLDER=""
